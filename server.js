5cc1514839587a1847285b80e9c1929db03bcb2f
JeffreyGrauzer <93175521+JeffreyGrauzer@users.noreply.github.com>
2022-01-18 18:26:51 -0600

    revised 11.1 file

--- server.js
+++ server.js
@@ -1,65 +1,74 @@
-const { animals } = require("./data/animals");
-const express = require("express");
-const PORT = process.env.PORT || 3001;
-const app = express();
-
-function filterByQuery(query, animalsArray) {
-  let personalityTraitsArray = [];
-  // Note that we save the animalsArray as filteredResults here:
-  let filteredResults = animalsArray;
-  if (query.personalityTraits) {
-    // Save personalityTraits as a dedicated array.
-    // If personalityTraits is a string, place it into a new array and save.
-    if (typeof query.personalityTraits === "string") {
-      personalityTraitsArray = [query.personalityTraits];
-    } else {
-      personalityTraitsArray = query.personalityTraits;
-    }
-    //Loop through each trait against each animal in the filteredResults array:
-    personalityTraitsArray.forEach((trait) => {
-      // Check the trait against each animal in the filteredResults array.
-      // Remember, it is initially a copy of the animalsArray
-      // but here we're updating it for each trait in the .forEach() loop.
-      // For each trait being targeted by the filter, the filteredResults
-      // array will then contain only the entries that contain the trait,
-      // so at the end we'll have an array of animals that have every one
-      // of the traits when the .forEach() loop is finished.
-      filteredResults = filteredResults.filter(
-        (animal) => animal.personalityTraits.indexOf(trait) !== -1
-      );
-    });
+{
+    "animals": [
+      {
+        "id": "0",
+        "name": "Spindle",
+        "species": "bear",
+        "diet": "omnivore",
+        "personalityTraits": ["hungry", "zany", "loving"]
+      },
+      {
+        "id": "1",
+        "name": "Terry",
+        "species": "gorilla",
+        "diet": "folivore",
+        "personalityTraits": ["anxious", "goofy"]
+      },
+      {
+        "id": "2",
+        "name": "Erica",
+        "species": "gorilla",
+        "diet": "folivore",
+        "personalityTraits": ["quirky", "rash"]
+      },
+      {
+        "id": "3",
+        "name": "Noel",
+        "species": "bear",
+        "diet": "omnivore",
+        "personalityTraits": ["impish", "sassy", "brave"]
+      },
+      {
+        "id": "4",
+        "name": "Coco",
+        "species": "penguin",
+        "diet": "piscivore",
+        "personalityTraits": ["loving", "goofy"]
+      },
+      {
+        "id": "5",
+        "name": "Marcy",
+        "species": "ostrich",
+        "diet": "omnivore",
+        "personalityTraits": ["hungry", "goofy"]
+      },
+      {
+        "id": "6",
+        "name": "Bodhi",
+        "species": "Doggo",
+        "diet": "omnivore",
+        "personalityTraits": ["zany"]
+      },
+      {
+        "id": "7",
+        "name": "Cassie",
+        "species": "Doggo",
+        "diet": "omnivore",
+        "personalityTraits": ["hungry", "zany", "goofy"]
+      },
+      {
+        "id": "8",
+        "name": "Roxy",
+        "species": "Quokka",
+        "diet": "herbivore",
+        "personalityTraits": ["loving"]
+      },
+      {
+        "id": "9",
+        "name": "Tamara",
+        "species": "Tiger",
+        "diet": "carnivore",
+        "personalityTraits": ["hungry"]
+      }
+    ]
   }
-
-  if (query.diet) {
-    filteredResults = filteredResults.filter(
-      (animal) => animal.diet === query.diet
-    );
-  }
-  if (query.species) {
-    filteredResults = filteredResults.filter(
-      (animal) => animal.species === query.species
-    );
-  }
-  if (query.name) {
-    filteredResults = filteredResults.filter(
-      (animal) => animal.name === query.name
-    );
-  }
-  // return the filtered results
-  return filteredResults;
-}
-function findById(id, animalsArray) {
-    const result = animalsArray.filter(animal => animal.id === id)[0];
-    return result;
-}
-app.get('/api/animals/:id', (req, res) => {
-  const result = findById(req.params.id, animals);
-  if (result) {
-      res.json(result);
-  } else {
-  res.send(404);
-  }
-});
-app.listen(PORT, () => {
-  console.log(`API server now on port ${PORT}!`);
-});
